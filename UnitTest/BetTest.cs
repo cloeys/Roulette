36015525a5445e767ef877a0a743a405961a1788
KevinEysackersC <32068330+KevinEysackersC@users.noreply.github.com>
2017-10-03 09:51:41 +0200

    Bet Unit test

--- UnitTest/BetTest.cs
+++ UnitTest/BetTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Roulette;
+using Roulette.Bets;
 
 namespace UnitTest
 {
@@ -7,8 +9,31 @@ namespace UnitTest
     public class BetTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculatePayout_SingleBet_Success()
         {
+            var amount = 10;
+            Tile tile = new Tile { Color = "red", Value = "1" };
+            Bet bet = new SingleBet(null, amount, tile);
+            bet.Tiles.Add(tile);
+            var winAmount = bet.CalculatePayout(tile);
+
+            Assert.AreEqual(360, winAmount);
         }
+
+
+        [TestMethod]
+        public void CalculatePayout_SingleBet_Fail()
+        {
+            var amount = 10;
+            Tile tileBetted = new Tile { Color = "red", Value = "1" };
+            Bet bet = new SingleBet(null, amount, tileBetted);
+            bet.Tiles.Add(tileBetted);
+
+            Tile tileWon = new Tile { Color = "red", Value = "10" };
+            var winAmount = bet.CalculatePayout(tileWon);
+
+            Assert.AreEqual(0, winAmount);
+        }
+
     }
 }
